c9b5b52751b14541420d82f29611e5d992313939
Lars Hampe <hello@hashdot.co>
2021-08-25 16:43:04 +0200

    episode comp: add readTime, wordCount and fix episode number

--- components/Episodes.tsx
+++ components/Episodes.tsx
@@ -8,7 +8,15 @@ export type Episode = {
     formatted_published_at: Date
     description: string
     media_url: string
+    number: number
   }
+}
+
+const getEpisodeNumber = ({ episode }: { episode: Episode }) => {
+  if (episode?.attributes?.number === null) {
+    return '000'
+  }
+  return String(episode?.attributes?.number).padStart(3, '0')
 }
 
 const Item = ({
@@ -29,21 +37,43 @@ const Item = ({
       )}
       onClick={() => setEpisodeId(episode.id)}
     >
-      <div className="text-xxs flex space-x-5 text-gray-400">
-        <p>Episode 000</p> <p>{episode?.attributes?.formatted_published_at}</p>
+      <div className="flex space-x-5 text-gray-400 text-xxs">
+        <p>Episode {getEpisodeNumber({ episode })}</p>{' '}
+        <p>{episode?.attributes?.formatted_published_at}</p>
       </div>
       <h1 className="text-xl">{episode?.attributes?.title}</h1>
     </div>
   )
 }
 
+const textAnalysis = ({ episode }: { episode: Episode }) => {
+  const wordsPerMinute = 200
+  const wordCount = episode?.attributes?.description?.trim().split(/\s+/).length
+  const readTime = Math.ceil(wordCount / wordsPerMinute)
+  return { readTime, wordCount }
+}
+
 const Summary = ({ episode }: { episode: Episode }) => {
   if (episode === undefined) {
     return null
   }
+  const { readTime, wordCount } = textAnalysis({ episode })
   return (
-    <div className="w-full flex flex-col bg-gray-900 px-5 py-2 rounded-md">
-      <h1 className="prose-xl text-gray-100 tracking-wider">Transcript</h1>
+    <div className="flex flex-col w-full px-5 py-2 bg-gray-900 rounded-md">
+      <div className="flex justify-between text-xs text-gray-500">
+        <p>Episode {getEpisodeNumber({ episode })}</p>{' '}
+        <p>{episode?.attributes?.formatted_published_at}</p>
+      </div>
+      <h1 className="mb-4 text-3xl text-gray-400">
+        {episode?.attributes?.title}
+      </h1>
+      <hr className="mt-2 mb-4" />
+      <div className="flex items-baseline justify-between mb-2">
+        <h2 className="tracking-wider prose-xl text-gray-100">Transcript</h2>
+        <span className="prose-sm text-gray-400">
+          Read Time: {readTime}min - Words: {wordCount}
+        </span>
+      </div>
       <div className="prose-lg text-gray-300">
         {parseHTML(episode?.attributes?.description)}
       </div>
@@ -63,7 +93,7 @@ const Episodes = ({
   episode: Episode
 }): JSX.Element => {
   return (
-    <div className="mt-16 flex w-full md:w-8/12 space-x-5 z-50">
+    <div className="z-50 flex w-full mt-16 space-x-5 md:w-8/12">
       <div className="w-full md:w-4/12">
         {episodes?.map((episode) => (
           <Item
@@ -74,7 +104,7 @@ const Episodes = ({
           />
         ))}
       </div>
-      <div className="w-full hidden md:block">
+      <div className="hidden w-full md:block">
         <Summary episode={episode} />
       </div>
     </div>
